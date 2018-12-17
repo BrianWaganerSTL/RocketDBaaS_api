4d49bb5fa6f2208da02942ff98177716dda5f0a1
Brian Waganer <BrianWaganerSTL@users.noreply.github.com>
2018-12-16 20:50:20 -0600

    Update getMetrics_MountPoints.py

--- dbaas/metrics/getMetrics_MountPoints.py
+++ dbaas/metrics/getMetrics_MountPoints.py
@@ -11,43 +11,61 @@ metrics_port = 8080
 
 def GetMetrics_MountPoints(server):
     print('Server='+str(server)+', ServerId='+str(server.id) + ', ServerIP=' + str(server.server_ip))
-    metrics_MountPoint = Metrics_MountPoint()
+
     url = 'http://' + server.server_ip + ':' + str(metrics_port) + '/api/metrics/mountpoints'
     print('Check: MountPoints, ServerNm: ' + server.server_name + ', url=' + url)
     metrics = ''
+    error_msg = ''
 
     try:
         r = requests.get(url)
+        print('r.status_code:' + str(r.status_code))
+        print('r.' + str(r.content))
         metrics = r.json()
+        print("metrics" + str(type(metrics)))
+        print(metrics)
         errCnt[server.id] = 0
     except requests.exceptions.ConnectionError:
         errCnt[server.id] = errCnt[server.id] + 1
-        metrics_MountPoint.error_msg = 'ConnectionRefusedError:  Make sure the Minion is up and running.'
+        error_msg = 'ConnectionRefusedError:  Make sure the Minion is up and running.'
     except requests.exceptions.Timeout:
         errCnt[server.id] = errCnt[server.id] + 1
-        metrics_MountPoint.error_msg = 'Timeout'
+        error_msg = 'Timeout'
     except requests.exceptions.TooManyRedirects:
         errCnt[server.id] = errCnt[server.id] + 1
-        metrics_MountPoint.error_msg = 'Bad URL'
+        error_msg = 'Bad URL'
     except requests.exceptions.RequestException as e:
         errCnt[server.id] = errCnt[server.id] + 1
-        metrics_MountPoint.error_msg = 'Catastrophic error. Bail ' + str(e)
-    except:
+        error_msg = 'Catastrophic error. Bail ' + str(e)
+    except requests.exceptions.HTTPError as err:
         errCnt[server.id] = errCnt[server.id] + 1
-        metrics_MountPoint.error_msg = 'Other Error '
+        error_msg = 'Other Error ' + err
 
-    metrics_MountPoint.server = server
-    metrics_MountPoint.error_cnt = errCnt[server.id]
-    if (metrics_MountPoint.error_msg == ''):
-        metrics_MountPoint.created_dttm = metrics['created_dttm']
-        metrics_MountPoint.mount_point = metrics['mount_point']
-        metrics_MountPoint.allocated_gb = metrics['allocated_gb']
-        metrics_MountPoint.used_gb = metrics['used_gb']
-        metrics_MountPoint.used_pct = metrics['used_pct']
-        metrics_MountPoint.save()
+    if (error_msg == ''):
+        for m in metrics:
+            print('m:' + str(m))
 
-        Track_MountPoints(server, metrics_MountPoint.id)
+            metrics_MountPoint = Metrics_MountPoint()
+            metrics_MountPoint.server = server
+            metrics_MountPoint.error_cnt = errCnt[server.id]
+            metrics_MountPoint.created_dttm = m['created_dttm']
+            metrics_MountPoint.mount_point = m['mount_point']
+            metrics_MountPoint.allocated_gb = m['allocated_gb']
+            metrics_MountPoint.used_gb = m['used_gb']
+            metrics_MountPoint.used_pct = m['used_pct']
+            metrics_MountPoint.save()
+
+            #try:
+            #     print('metrics_MountPoint.id:' + metrics_MountPoint.id)
+            #     Track_MountPoints(server, metrics_MountPoint.id)
+            # except:
+            #     print('ERROR: ' + str(e))
+            #     pass
     else:
+        metrics_MountPoint = Metrics_MountPoint()
+        metrics_MountPoint.server = server
+        metrics_MountPoint.error_cnt = errCnt[server.id]
         metrics_MountPoint.created_dttm = timezone.now()
+        metrics_MountPoint.error_msg = error_msg
         metrics_MountPoint.save()
 
