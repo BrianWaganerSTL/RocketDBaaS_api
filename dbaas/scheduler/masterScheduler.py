db177f0a3a3de3256f574fdd33c029e2b1720400
Brian Waganer <BrianWaganerSTL@users.noreply.github.com>
2018-12-18 15:05:28 -0600

    Update masterScheduler.py

--- dbaas/scheduler/masterScheduler.py
+++ dbaas/scheduler/masterScheduler.py
@@ -1,13 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.utils import timezone
-
+import sys
 from dbaas.metrics.collectMetrics import MetricsFastTick, MetricsSlowTick
 
 
 def StartMasterScheduler():
-    print('This is the Start of all Good and Wonderful things...')
-
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(MetricsFastTick, 'interval', seconds=45, max_instances=10, next_run_time=timezone.now())
-    scheduler.add_job(MetricsSlowTick, 'interval', seconds=120, max_instances=5, next_run_time=timezone.now())
-    scheduler.start()
+    if "manage.py" not in sys.argv[0]:
+        print('This is the Start of all Good and Wonderful things...')
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(MetricsFastTick, 'interval', seconds=45, max_instances=10, next_run_time=timezone.now())
+        scheduler.add_job(MetricsSlowTick, 'interval', seconds=120, max_instances=5, next_run_time=timezone.now())
+        scheduler.start()
+    else:
+        print('Schedulers NOT started')
