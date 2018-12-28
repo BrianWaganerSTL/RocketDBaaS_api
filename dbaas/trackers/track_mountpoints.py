1eb55c627e07f02e77b3ddf99af576b4fa5c2d39
Brian Waganer <BrianWaganerSTL@users.noreply.github.com>
2018-12-28 11:34:45 -0600

    Update track_mountpoints.py

--- dbaas/trackers/track_mountpoints.py
+++ dbaas/trackers/track_mountpoints.py
@@ -1,24 +1,24 @@
 import logging
 
+from django.core.mail import EmailMessage
 from django.utils import timezone
 
 from dbaas.models import IssueNotification, Server, Metrics_MountPoint, IssueTracker, IssueStatusChoices, ApplicationContact, CheckerThreshold
+from dbaas.trackers.SendNotificationEmail import send_notification_email
 from dbaas.utils.DynCompare import DynCompare
 
-mySubjectTemplate = '''<span style="%s">%s</span>: Server:%s,
-    %s: %s, 
-    Current Test: %s (%s)
-    '''
-myHtmlTemplate = '''<h1><span style="%s">%s</span> Server: %s</h1>\n
+mySubjectTemplate = '%s: Server: %s, %s: %s, Current Test: %s (%s)'
+myHtmlTemplate = '''<h1 style="%s; margin:0px; padding:1rem">Server: %s &nbsp;&nbsp;&nbsp; <span style="text-decoration: underline">%s</span></h1>
+    <br>\n
     %s: %s <br>\n
     <br>\n
-    Currently Value: %s %s<br>\n
+    Current Value: %s %s<br>\n
     Current Test: (%s) <br>\n
     <br>\n
     Started Tracking: %s <br>\n
-    Current DateTime: %s <br>\n
+    Last Activity: %s <br>\n
     <br>\n
-    IssueTrackerId: %s <br>\n
+    Issue Tracker ID: %s <br>\n
     <br>\n
     Link to Issue: <a href="http://0.0.0.0:8000/clusters/%s/servers/%s/issuetrackers/%s">Click</a> <br>\n
     '''
@@ -33,9 +33,6 @@ class Tracker:
         self.checkerThreshold = checkerThreshold
         self.thresholdLevel = thresholdLevel
         self.thresholdTestWithValues = thresholdTestWithValues
-
-
-
 
 
 def Track_MountPoints(slimServer, metrics_MountPoint_id):
@@ -82,6 +79,8 @@ def UpdIssueTracker(myTracker):
                                         checker_threshold=myTracker.checkerThreshold,
                                         pending_status=myTracker.thresholdLevel)
             print('Created a IssueTracker')
+        else:
+            return
 
     t = issueTracker
     t.save()  # Save so I get the datetimes and other default
@@ -94,7 +93,7 @@ def UpdIssueTracker(myTracker):
             if (t.pending_status != t.current_status):
                 twerkIt = True
                 t.current_status = t.pending_status
-                headerColor = 'color:red'
+                headerColor = 'background-color:red'
             t.pending_status = IssueStatusChoices.Critical
 
     elif myTracker.thresholdLevel == 'Warning':
@@ -106,7 +105,7 @@ def UpdIssueTracker(myTracker):
             if (t.pending_status != t.current_status):
                 twerkIt = True
                 t.current_status = t.pending_status
-                headerColor = 'color:orange'
+                headerColor = 'background-color:orange'
             t.current_status = IssueStatusChoices.Warning
 
     elif myTracker.thresholdLevel == 'Normal':
@@ -119,7 +118,7 @@ def UpdIssueTracker(myTracker):
                 twerkIt = True
                 t.current_status = t.pending_status
                 t.closed_sw = True
-                headerColor = 'color:green'
+                headerColor = 'background-color:green'
                 t.current_status = IssueStatusChoices.Normal
     t.save()
     print('mount_point: ' + myTracker.metricsMountPoint.mount_point)
@@ -128,13 +127,13 @@ def UpdIssueTracker(myTracker):
         print('============================================')
         print('Create a new Issue Notification and TwerkIt!')
         mySubject = mySubjectTemplate % (
-            headerColor, myTracker.thresholdLevel, t.server.server_name,
+            myTracker.thresholdLevel, t.server.server_name,
             myTracker.checkerThreshold.checker_base_element.metric_name,
             myTracker.metricsMountPoint.mount_point,
             myTracker.checkerThreshold.checker_base_element.metric_element,
             myTracker.thresholdTestWithValues)
         myBody = myHtmlTemplate % (
-            headerColor, myTracker.thresholdLevel, t.server.server_name,
+            headerColor, t.server.server_name, myTracker.thresholdLevel,
             myTracker.checkerThreshold.checker_base_element.metric_name,
             myTracker.metricsMountPoint.mount_point,
             myTracker.checkerThreshold.checker_base_element.metric_element,
@@ -144,7 +143,6 @@ def UpdIssueTracker(myTracker):
             timezone.datetime.strftime(t.last_dttm, "%a %b %d, %Y %H:%M"),
             t.id,
             t.server.cluster_id, t.server_id, t.id)
-        print('mount_point: ' + myTracker.metricsMountPoint.mount_point)
 
         i = IssueNotification(issue_tracker_id=t.id,
                               application=t.server.cluster.application,
@@ -153,12 +151,4 @@ def UpdIssueTracker(myTracker):
                               notification_body=myBody)
         i.save()
 
-        print('Notify the following contacts')
-        for ac in ApplicationContact.objects.filter(application=t.server.cluster.application, contact__active_sw=True):
-            print('  %s: email: %s, phone: %s' % (
-                ac.contact.contact_name,
-                ac.contact.contact_email,
-                ac.contact.contact_phone))
-        print('============================================\n')
-
-
+        SendNotificationEmail(i.id)
