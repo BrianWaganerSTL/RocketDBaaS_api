13453cafd2c2af9066f9fe80ec1e069d21e56eef
Brian Waganer <BrianWaganerSTL@users.noreply.github.com>
2018-12-28 11:18:51 -0600

    alerts, and less redundant code

--- dbaas/metrics/getMetrics_Cpu.py
+++ dbaas/metrics/getMetrics_Cpu.py
@@ -1,52 +1,83 @@
-# This calls out to the server to ask for the data, then pulls it back and saves it.
 from datetime import datetime
 import time
 import os
 import requests
+from django.utils import timezone
 
 from dbaas.models import MetricsCpu
+from dbaas.trackers.track_cpu import Track_Cpu
 
 errCnt = [0] * 1000
 metrics_port = 8080
 
 
-def GetMetricsCpu(s):
-    url = 'http://' + s.server_ip + ':' + str(metrics_port) + '/api/metrics/cpu'
-    print('Cpu: ServerNm: ' + s.server_name + ', url=' + url)
+def GetMetricsCpu(server):
+    print('Server=' + str(server) + ', ServerId=' + str(server.id) + ', ServerIP=' + str(server.server_ip))
 
-    metricsCpu = MetricsCpu()
+    url = 'http://' + server.server_ip + ':' + str(metrics_port) + '/api/metrics/cpu'
+    print('Cpu: ServerNm: ' + server.server_name + ', url=' + url)
+    metrics = ''
     error_msg = ''
+
     try:
         r = requests.get(url)
+        print('r.status_code:' + str(r.status_code))
+        print('r.' + str(r.content))
         metrics = r.json()
+        print("metrics" + str(type(metrics)) + ', Count=' + str(len(metrics)))
         print(metrics)
+        errCnt[server.id] = 0
 
-        errCnt[s.id] = errCnt[s.id] = 0
-        metricsCpu.created_dttm = metrics['created_dttm']
-        metricsCpu.cpu_idle_pct = metrics['idle']
-        metricsCpu.cpu_user_pct = metrics['user']
-        metricsCpu.cpu_system_pct = metrics['system']
-        if 'cpu_iowait_pct' in metrics:  # These only exist in Unix/Linux
-            metricsCpu.cpu_iowait_pct = metrics['cpu_iowait_pct']
-            metricsCpu.cpu_irq_pct = metrics['cpu_irq_pct']
-            metricsCpu.cpu_steal_pct = metrics['cpu_steal_pct']
-            if 'cpu_guest_pct' in metrics:  # Only certain versions of Unix/Linux has
-                metricsCpu.cpu_guest_pct = metrics['cpu_guest_pct']
-                metricsCpu.cpu_guest_nice_pct = metrics['cpu_guest_nice_pct']
+    except requests.exceptions.ConnectionError:
+        errCnt[server.id] = errCnt[server.id] + 1
+        error_msg = 'ConnectionRefusedError:  Make sure the Minion is up and running.'
     except requests.exceptions.Timeout:
-        errCnt[s.id] = errCnt[s.id] + 1
+        errCnt[server.id] = errCnt[server.id] + 1
         error_msg = 'Timeout'
     except requests.exceptions.TooManyRedirects:
-        errCnt[s.id] = errCnt[s.id] + 1
+        errCnt[server.id] = errCnt[server.id] + 1
         error_msg = 'Bad URL'
     except requests.exceptions.RequestException as e:
-        errCnt[s.id] = errCnt[s.id] + 1
+        errCnt[server.id] = errCnt[server.id] + 1
         error_msg = 'Catastrophic error. Bail ' + str(e)
     except requests.exceptions.HTTPError as err:
-        errCnt[s.id] = errCnt[s.id] + 1
+        errCnt[server.id] = errCnt[server.id] + 1
         error_msg = 'Other Error ' + err
 
-    metricsCpu.error_msg = error_msg
-    metricsCpu.error_cnt = errCnt[s.id]
-    metricsCpu.server = s
-    metricsCpu.save()
+    if (error_msg == ''):
+        if (type(metrics) == dict):
+            metricsList = [metrics]
+        else:
+            metricsList = metrics
+
+        for m in metricsList:
+            print('m:' + str(m))
+
+            metricsCpu = MetricsCpu()
+            metricsCpu.server = server
+            metricsCpu.error_cnt = errCnt[server.id]
+            metricsCpu.created_dttm = m['created_dttm']
+            metricsCpu.cpu_idle_pct = metrics['idle']
+            metricsCpu.cpu_user_pct = metrics['user']
+            metricsCpu.cpu_system_pct = metrics['system']
+            if 'cpu_iowait_pct' in metrics:  # These only exist in Unix/Linux
+                metricsCpu.cpu_iowait_pct = metrics['cpu_iowait_pct']
+                metricsCpu.cpu_irq_pct = metrics['cpu_irq_pct']
+                metricsCpu.cpu_steal_pct = metrics['cpu_steal_pct']
+                if 'cpu_guest_pct' in metrics:  # Only certain versions of Unix/Linux has
+                    metricsCpu.cpu_guest_pct = metrics['cpu_guest_pct']
+                    metricsCpu.cpu_guest_nice_pct = metrics['cpu_guest_nice_pct']
+            metricsCpu.save()
+
+            try:
+                 Track_Cpu(server, metricsCpu.id)
+            except:
+                 print('ERROR: ' + str(e))
+                 pass
+    else:
+        metricsCpu = MetricsCpu()
+        metricsCpu.server = server
+        metricsCpu.error_cnt = errCnt[server.id]
+        metricsCpu.created_dttm = timezone.now()
+        metricsCpu.error_msg = error_msg
+        metricsCpu.save()
