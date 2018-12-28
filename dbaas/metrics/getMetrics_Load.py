53849b65a2c80fd720e356f9358093bc0f1a537f
Brian Waganer <BrianWaganerSTL@users.noreply.github.com>
2018-12-28 11:20:17 -0600

    alerts and less redundant code

--- dbaas/metrics/getMetrics_Load.py
+++ dbaas/metrics/getMetrics_Load.py
@@ -5,49 +5,73 @@ import requests
 from django.utils import timezone
 
 from dbaas.models import MetricsLoad
+from dbaas.trackers.track_load import Track_Load
 
 errCnt = [0] * 1000
 metrics_port = 8080
 
 
-def GetMetricsLoad(s):
-    metricsLoad = MetricsLoad()
-    url = 'http://' + s.server_ip + ':' + str(metrics_port) + '/api/metrics/load'
+def GetMetricsLoad(server):
+    print('Server=' + str(server) + ', ServerId=' + str(server.id) + ', ServerIP=' + str(server.server_ip))
+
+    url = 'http://' + server.server_ip + ':' + str(metrics_port) + '/api/metrics/load'
     print('Load: url=' + url)
+    metrics = ''
+    error_msg = ''
+
     try:
         r = requests.get(url)
+        print('r.status_code:' + str(r.status_code))
+        print('r.' + str(r.content))
         metrics = r.json()
+        print("metrics" + str(type(metrics)) + ', Count=' + str(len(metrics)))
         print(metrics)
+        errCnt[server.id] = 0
 
-        error_cnt = 0
-        metricsLoad.server = s
-        metricsLoad.created_dttm = metrics['created_dttm']
-        metricsLoad.load_1min = metrics['load_1min']
-        metricsLoad.load_5min = metrics['load_5min']
-        metricsLoad.load_15min = metrics['load_15min']
-        metricsLoad.error_cnt = error_cnt
-        metricsLoad.save()
+    except requests.exceptions.ConnectionError:
+        errCnt[server.id] = errCnt[server.id] + 1
+        error_msg = 'ConnectionRefusedError:  Make sure the Minion is up and running.'
     except requests.exceptions.Timeout:
-        errCnt[s.id] = errCnt[s.id] + 1
-        metricsLoad.server = s
-        metricsLoad.error_cnt = errCnt[s.id]
-        metricsLoad.error_msg = 'Timeout'
-        metricsLoad.save()
+        errCnt[server.id] = errCnt[server.id] + 1
+        error_msg = 'Timeout'
     except requests.exceptions.TooManyRedirects:
-        errCnt[s.id] = errCnt[s.id] + 1
-        metricsLoad.server = s
-        metricsLoad.error_cnt = errCnt[s.id]
-        metricsLoad.error_msg = 'Bad URL'
-        metricsLoad.save()
+        errCnt[server.id] = errCnt[server.id] + 1
+        error_msg = 'Bad URL'
     except requests.exceptions.RequestException as e:
-        errCnt[s.id] = errCnt[s.id] + 1
-        metricsLoad.server = s
-        metricsLoad.error_cnt = errCnt[s.id]
-        metricsLoad.error_msg = 'Catastrophic error. Bail ' + str(e)
-        metricsLoad.save()
+        errCnt[server.id] = errCnt[server.id] + 1
+        error_msg = 'Catastrophic error. Bail ' + str(e)
     except requests.exceptions.HTTPError as err:
-        errCnt[s.id] = errCnt[s.id] + 1
-        metricsLoad.server = s
-        metricsLoad.error_cnt = errCnt[s.id]
-        metricsLoad.error_msg = 'Other Error ' + err
+        errCnt[server.id] = errCnt[server.id] + 1
+        error_msg = 'Other Error ' + err
+
+    if (error_msg == ''):
+        if (type(metrics) == dict):
+            metricsList = [metrics]
+        else:
+            metricsList = metrics
+
+        for m in metricsList:
+            print('m:' + str(m))
+
+            metricsLoad = MetricsLoad()
+            metricsLoad.server = server
+            metricsLoad.error_cnt = errCnt[server.id]
+            metricsLoad.created_dttm = m['created_dttm']
+            metricsLoad.load_1min = m['load_1min']
+            metricsLoad.load_5min = m['load_5min']
+            metricsLoad.load_15min = m['load_15min']
+            metricsLoad.save()
+
+            try:
+                 print('metricsLoad.id: ' + str(metricsLoad.id))
+                 Track_Load(server, metricsLoad.id)
+            except:
+                 print('ERROR: ' + str(e))
+                 pass
+    else:
+        metricsLoad = MetricsLoad()
+        metricsLoad.server = server
+        metricsLoad.error_cnt = errCnt[server.id]
+        metricsLoad.created_dttm = timezone.now()
+        metricsLoad.error_msg = error_msg
         metricsLoad.save()
